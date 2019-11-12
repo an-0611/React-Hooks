de554788aa72092d9c15411aab471a0338643b9f
An <tt50914@settour.com.uw>
2019-11-12 13:48:15 +0800

    Updates

---
+++ precache-manifest.24289c9eaacdd5243d7c631174ca28d6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87a0ad802828c73298e57395e50e771e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "76e7fcf0357357d16180",
+    "url": "./static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "5e28c71829a6337b7b61",
+    "url": "./static/js/2.5c5423cd.chunk.js"
+  },
+  {
+    "revision": "76e7fcf0357357d16180",
+    "url": "./static/js/main.2b5e5c5a.chunk.js"
+  },
+  {
+    "revision": "9b8470acda6259e09f64",
+    "url": "./static/js/runtime-main.76aec09b.js"
+  }
+]);
